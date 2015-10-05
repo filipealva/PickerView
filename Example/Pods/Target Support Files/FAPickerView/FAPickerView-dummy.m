5adb72b0b3808c401e98db9917909d5ca08faaa4
filipealva <ofilipealvarenga@gmail.com>
2015-10-05 12:39:28 -0300

    Remove old project

--- Example/Pods/Target Support Files/FAPickerView/FAPickerView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_FAPickerView : NSObject
-@end
-@implementation PodsDummy_FAPickerView
-@end
--- Example/Pods/Target Support Files/FAPickerView/FAPickerView-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double FAPickerViewVersionNumber;
-FOUNDATION_EXPORT const unsigned char FAPickerViewVersionString[];
-
